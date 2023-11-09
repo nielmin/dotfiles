3e4b4a40ec9d03b8ee2ae9aac61e4a4daf5cd1d9
nielmin <whadmia@gmail.com>
2023-11-09 15:33:03 -0600

    Remove auto-compile from startup

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -5,14 +5,11 @@
 
 (setq load-prefer-newer t)
 
-(require 'auto-compile)
-(auto-compile-on-load-mode)
-(auto-compile-on-save-mode)
-
 (org-babel-load-file
  (expand-file-name
   "config.org"
   user-emacs-directory))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -21,7 +18,7 @@
  '(custom-safe-themes
    '("75fb82e748f32de807b3f9e8c72de801fdaeeb73c791f405d8f73711d0710856" default))
  '(package-selected-packages
-   '(visual-fill-column catppuccin-theme cratppuccin-theme bufler eglot which-key vertico rainbow-delimiters org-superstar orderless meow marginalia doom-themes doom-modeline diminish dashboard consult company beacon auto-package-update auto-compile)))
+   '(jinx visual-fill-column catppuccin-theme cratppuccin-theme bufler eglot which-key vertico rainbow-delimiters org-superstar orderless meow marginalia doom-themes doom-modeline diminish dashboard consult company beacon auto-package-update auto-compile)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
