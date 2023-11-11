9583c64b5fe4a0de1f57ac473e32017a3f8ca887
nielmin <whadmia@gmail.com>
2023-11-10 19:43:49 -0600

    Move auto-compile from init.el to config.org

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,12 +1,5 @@
 (require 'package)
-(setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
-
-(setq load-prefer-newer t)
-(require 'auto-compile)
-(auto-compile-on-load-mode)
-(auto-compile-on-save-mode)
 
 (org-babel-load-file
  (expand-file-name
