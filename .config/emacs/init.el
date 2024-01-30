1c233254b084de30f881958d6ee9e7bfe2c28355
daniel <whadmia@gmail.com>
2024-01-30 15:51:27 -0600

    Update init.el

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -13,7 +13,7 @@
  '(custom-safe-themes
    '("f5661fd54b1e60a4ae373850447efc4158c23b1c7c9d65aa1295a606278da0f8" default))
  '(package-selected-packages
-   '(modus-themes auto-compile which-key vertico rainbow-delimiters org-superstar orderless meow marginalia magit jinx doom-themes doom-modeline diminish consult company bufler beacon auto-package-update)))
+   '(citeproc modus-themes auto-compile which-key vertico rainbow-delimiters org-superstar orderless meow marginalia magit jinx doom-themes doom-modeline diminish consult company bufler beacon auto-package-update)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
