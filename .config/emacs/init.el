5050057a208b25f2037316d52fd535a937e1a2f4
daniel <whadmia@gmail.com>
2024-02-06 02:05:19 -0600

    Update init.el

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -5,18 +5,3 @@
  (expand-file-name
   "config.org"
   user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("f5661fd54b1e60a4ae373850447efc4158c23b1c7c9d65aa1295a606278da0f8" default))
- '(package-selected-packages
-   '(undo-tree tree-sitter embark-consult embark citeproc modus-themes auto-compile which-key vertico rainbow-delimiters org-superstar orderless meow marginalia magit jinx doom-themes doom-modeline diminish consult company bufler beacon auto-package-update)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
