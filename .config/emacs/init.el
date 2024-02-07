47d371d25857cb95904c5560b9f27f390393f153
daniel <whadmia@gmail.com>
2024-02-07 00:17:06 -0600

    Update init.el

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,7 +1,14 @@
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+            '("melpa" . "https://melpa.org/packages/")
+            '("gnu" . "https://elpa.gnu.org/packages/")
+            )
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-and-compile
+  (setq use-package-always-ensure t
+        use-package-expand-minimally t))
 
 (org-babel-load-file
- (expand-file-name
-  "config.org"
-  user-emacs-directory))
+ (expand-file-name "config.org" user-emacs-directory))
