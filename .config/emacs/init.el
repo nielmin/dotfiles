1d72ae40d3b89887d9403b6aceb72fdb44718272
daniel <whadmia@gmail.com>
2024-02-20 00:32:57 -0600

    Update init.el with custom-set-variables

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -75,3 +75,16 @@
 
 (org-babel-load-file
  (expand-file-name "config.org" user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(which-key vertico undo-tree tree-sitter rainbow-delimiters org-superstar orderless modus-themes meow marginalia magit jinx embark-consult doom-themes doom-modeline diminish company citeproc auto-package-update auto-compile)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
