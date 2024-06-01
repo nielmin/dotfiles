a3074643f862131b01ed8cae084c909d95749e0b
daniel <whadmia@gmail.com>
2024-06-01 18:15:47 -0500

    Update tree-sitter module

--- .config/nvim/lua/foo/plugins/tree-sitter.lua
+++ .config/nvim/lua/foo/plugins/tree-sitter.lua
@@ -1,22 +1,36 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter", 
-        build = ":TSUpdate",
-        config = function ()
-            local configs = require("nvim-treesitter.configs")
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+      "windwp/nvim-ts-autotag",
+    },
+    config = function ()
+      local configs = require("nvim-treesitter.configs")
 
-            configs.setup({
-                ensure_installed = { 
-                    "c",
-                    "lua",
-                    "html",
-                    "python",
-                    "go"
-                },
-                sync_install = false,
-                higlight = { enable = true },
-                indent = { enable = true },
-            })
-        end
-    }
+      configs.setup({
+        highlight = { enable = true },
+        indent = { enable = true },
+        autotag = { enable = true },
+        ensure_installed = {
+          "c",
+          "lua",
+          "html",
+          "python",
+          "go"
+        },
+        sync_install = false,
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          },
+        },
+      })
+    end
+  }
 }
