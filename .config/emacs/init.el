bde801dd4ff4f47452caecd9ad5ce722a01352fa
daniel <whadmia@gmail.com>
2024-06-01 18:20:36 -0500

    Update init.el by Custom

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -89,3 +89,16 @@
         web-mode-css-indent-offset 2
         web-mode-code-indent-offset 2)
   )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(magit which-key web-mode vertico tsc treesit-auto rainbow-delimiters org-superstar org-ref org-modern orderless modus-themes meow marginalia jinx flycheck diminish consult company auto-compile)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
