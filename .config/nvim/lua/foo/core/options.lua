a625671b6085e4ce6a0d8d1ff20b2c5b3b4babe6
daniel <whadmia@gmail.com>
2024-06-01 18:13:34 -0500

    Update neovim options

--- .config/nvim/lua/foo/core/options.lua
+++ .config/nvim/lua/foo/core/options.lua
@@ -1,14 +1,39 @@
-local opt = vim.opt
+vim.cmd("let g:netrw_liststyle = 3")
+
+local opt = vim.opt -- for conciseness
+
+-- line numbers
 opt.nu = true
 opt.rnu = true
-opt.shiftwidth = 4
-opt.tabstop = 4
+
+-- tabs & indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
 opt.expandtab = true
-opt.backup = false
+opt.autoindent = true
+opt.smartindent = true
+
+-- scroll
 opt.scrolloff = 10
+
+-- search settings
 opt.ignorecase = true
 opt.smartcase = true
-opt.smartindent = true
-opt.swapfile = false
+
+-- cursor line & column
 opt.cul = true
 opt.colorcolumn = '80'
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+-- opt.clipboard:append("unnamedplus")
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
+
+-- no generated files
+opt.backup = false
+opt.swapfile = false
