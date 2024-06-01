697d7c47a76435bfd5da4ea7012a08713c499672
daniel <whadmia@gmail.com>
2024-06-01 18:14:57 -0500

    Add additional path to Lazy

--- .config/nvim/lua/foo/lazy.lua
+++ .config/nvim/lua/foo/lazy.lua
@@ -10,4 +10,16 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("foo.plugins")
+
+require("lazy").setup({
+  { import = "foo.plugins" },
+  { import = "foo.plugins.lsp" }
+}, {
+    checker = {
+      enabled = true,
+      notify = false,
+    },
+    change_detection = {
+      notify = false,
+    },
+  })
