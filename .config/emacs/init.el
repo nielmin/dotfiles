7203048c89675f10477b4169035b15e6ea4457ff
Daniel Hwang <daniel@ines.localdomain>
2024-08-13 05:53:44 -0500

    Update package-selected-packages in init.el

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -90,3 +90,16 @@
 
 (use-package magit
  )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(which-key vertico treesit-auto rainbow-delimiters org-modern orderless meow marginalia magit flycheck ef-themes diminish consult company)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
