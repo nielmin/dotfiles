347292ec2398ab295fcc371977e84986e1a3a859
Daniel Hwang <daniel@ines.localdomain>
2024-08-24 18:54:02 -0500

    Add default magit binds

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -97,3 +97,16 @@
   :bind (("C-x g" . magit-status)
          ("C-x C-g" . magit-status))
   )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(magit which-key web-mode vertico treesit-auto rainbow-delimiters org-modern orderless meow marginalia magit-section git-commit flycheck ef-themes diminish consult company)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
