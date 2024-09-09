96ec029a15b9a7ca4b20a4200f10ffac5ec60ffc
Daniel Hwang <dawh@fedora>
2024-09-09 12:21:22 -0500

    Refactor dependencies

--- .config/nvim/lua/foo/plugins/telescope.lua
+++ .config/nvim/lua/foo/plugins/telescope.lua
@@ -2,8 +2,8 @@ return {
   'nvim-telescope/telescope.nvim', branch = '0.1.x',
   dependencies = { 
     'nvim-lua/plenary.nvim',
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     'nvim-tree/nvim-web-devicons',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   },
   config = function ()
     local telescope = require("telescope")
