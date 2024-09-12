f9b07fad24d167098a55efcfa0806ce73d9fd8dd
Daniel Hwang <daniel@ines.localdomain>
2024-09-12 18:38:41 -0500

    Change theme from catppuccin to rose-pine

--- .config/nvim/lua/foo/plugins/colorscheme.lua
+++ .config/nvim/lua/foo/plugins/colorscheme.lua
@@ -1,11 +1,18 @@
 return {
     -- catppuccin
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-	config = function()
-		vim.cmd.colorscheme('catppuccin')
-        end,
+    -- {
+    --     "catppuccin/nvim",
+    --     name = "catppuccin",
+    --     priority = 1000,
+	-- config = function()
+	-- 	vim.cmd.colorscheme('catppuccin')
+    --     end,
+    -- },
+    { 
+        "rose-pine/neovim", 
+        name = "rose-pine",
+    config = function() 
+        vim.cmd.colorscheme('rose-pine-moon')
+            end,
     },
 }
