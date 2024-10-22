0681c38583538a093a9bcc303375a95658a20a8c
Daniel Hwang <daniel@ines.localdomain>
2024-10-22 18:22:07 -0500

    Add conform.nvim config

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,4 +1,29 @@
 return {
     'stevearc/conform.nvim',
-    opts = {}
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+        {
+            "<leader>f",
+            function()
+                require("conform").format({ async = true})
+            end,
+            mode = "",
+            desc = "Format buffer",
+        },
+    },
+    ---@module "conform"
+    ---@type conform.setupOpts
+    opts = {
+        -- Define your formatters
+        formatters_by_ft = {
+            go = { "goimports", "gofmt" },
+        },
+        -- Set default options
+        default_format_opts = {
+            lsp_format = "fallback",
+        },
+        -- Set up format-on-save
+        format_on_save = { timeout_ms = 500 },
+    },
 }
