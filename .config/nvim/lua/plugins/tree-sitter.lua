2e6458ceb89e58d41dc0bb58e1674872db9d88ea
Daniel Hwang <dawh@ines.home>
2025-01-09 18:55:36 -0600

    Remove html and python modules

--- .config/nvim/lua/plugins/tree-sitter.lua
+++ .config/nvim/lua/plugins/tree-sitter.lua
@@ -1,36 +1,28 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
-    build = ":TSUpdate",
-    dependencies = {
-      "windwp/nvim-ts-autotag",
-    },
-    config = function ()
-      local configs = require("nvim-treesitter.configs")
+    {
+        "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPre", "BufNewFile" },
+        build = ":TSUpdate",
+        dependencies = {
+            "windwp/nvim-ts-autotag",
+        },
+        config = function()
+            local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-        highlight = { enable = true },
-        indent = { enable = true },
-        autotag = { enable = true },
-        ensure_installed = {
-          "c",
-          "lua",
-          "html",
-          "python",
-          "go"
-        },
-        sync_install = false,
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<C-space>",
-            node_incremental = "<C-space>",
-            scope_incremental = false,
-            node_decremental = "<bs>",
-          },
-        },
-      })
-    end
-  }
+            configs.setup({
+                highlight = { enable = true },
+                indent = { enable = true },
+                autotag = { enable = true },
+                ensure_installed = {
+                    "c",
+                    "lua",
+                    "go"
+                },
+                sync_install = false,
+                incremental_selection = {
+                    enable = true,
+                },
+            })
+        end
+    }
 }
