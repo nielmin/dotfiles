dbd8b90a639e56a81e2a5c0b7ceaec87bcf0f29d
Daniel Hwang <dawh@ines.home>
2025-03-24 01:12:01 -0500

    Add alias for podlet container

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -5,3 +5,7 @@ end
 function butane
   command podman run -i --rm -v "$PWD":/pwd quay.io/coreos/butane:release $argv
 end
+
+function podlet
+  command podman run -it --rm -v "$PWD":/pwd ghcr.io/containers/podlet $argv
+end
