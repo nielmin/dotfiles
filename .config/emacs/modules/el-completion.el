a87f7d7a1b874a6d3b3244a1d5f4c45d64c51bc0
Daniel Hwang <dawh@ines.home>
2025-04-18 17:43:57 -0500

    Update corfu config

--- .config/emacs/modules/el-completion.el
+++ .config/emacs/modules/el-completion.el
@@ -32,7 +32,19 @@
      )
 
   (use-package corfu
-    :init
-    (global-corfu-mode)
+    :ensure t
+    :hook (after-init . global-corfu-mode)
+    :bind (:map corfu-map ("<tab>" . corfu-complete))
+    :config
+    (setopt tab-alwals-indent 'complete)
+    (setopt corfu-preview-current nil)
+    (setopt corfu-min-width 20)
+
+    (setopt corfu-popupinto-delay '(1.25 . 0.5))
+    (corfu-popupinfo-mode 1)
+
+    (with-eval-after-load 'savehist
+      (corfu-history-mode 1)
+      (add-to-list 'savehist-additional-variables 'corfu-history))
     )
   (provide 'el-completion)
