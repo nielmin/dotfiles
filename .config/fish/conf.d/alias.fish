cf716a4872c5f73fef7b13d31df50c343f5de4ba
Daniel Hwang <dawh@ines.home>
2025-05-07 17:47:26 -0500

    Add ll alias

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -2,6 +2,10 @@ function ls
   command ls --color=auto $argv
 end
 
+function ll
+  command ls -ahl --color=auto $argv
+end
+
 function butane
   command podman run -i --rm -v "$PWD":/pwd quay.io/coreos/butane:release $argv
 end
