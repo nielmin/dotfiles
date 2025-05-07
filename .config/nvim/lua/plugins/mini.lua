6746bd27add5b2a26e8ba55903a65a05b60a9970
Daniel Hwang <dawh@ines.home>
2025-05-07 17:18:43 -0500

    Add keybinds for mini.pick

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -11,5 +11,10 @@ return {
     require('mini.completion').setup()
 
     require('mini.pick').setup()
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
+
+    keymap.set("n", "<leader>ff", "<cmd>Pick files<cr>", { desc = "Search files" })
+    keymap.set("n", "<leader>fs", "<cmd>Pick grep_live<cr>", { desc = "Search string in files" })
   end,
 }
