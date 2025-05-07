69e2d83354b731331a97d2103f0ddc5ab83571e3
Daniel Hwang <dawh@ines.home>
2025-05-07 16:55:49 -0500

    Replace lualine with mini.statusline

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -2,6 +2,9 @@ return {
   'echasnovski/mini.nvim',
   version = false,
   config = function()
-
+    require('mini.icons').setup()
+    require('mini.statusline').setup()
+    require('mini.git').setup()
+    require('mini.diff').setup()
   end,
 }
