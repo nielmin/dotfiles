4cf80b9fc1ae70c0927b462fae4ef9000a054ed5
Daniel Hwang <dawh@ines.home>
2025-05-08 00:33:14 -0500

    Remove unused colorschemes

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,29 +1,4 @@
 return {
-  -- catppuccin
-  -- {
-  --     "catppuccin/nvim",
-  --     name = "catppuccin",
-  --     priority = 1000,
-  -- config = function()
-  -- 	vim.cmd.colorscheme('catppuccin')
-  --     end,
-  -- },
-  -- {
-  --     "rose-pine/neovim",
-  --     name = "rose-pine",
-  -- config = function()
-  --     vim.cmd.colorscheme('rose-pine-moon')
-  --         end,
-  -- },
-  -- {
-  --     "folke/tokyonight.nvim",
-  --     lazy = false,
-  --     priority = 1000,
-  --     name = "tokyonight",
-  -- config = function()
-  --     vim.cmd.colorscheme('tokyonight')
-  --         end,
-  -- },
   {
     "webhooked/kanso.nvim",
     lazy = false,
