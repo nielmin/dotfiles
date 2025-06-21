e3a00d90c364a69ea4e200cde996f81bc18ba176
Daniel Hwang <dawh@ines.home>
2025-06-21 14:49:00 -0500

    Update nvim theme to zenbones

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,11 @@
 return {
   {
-    "webhooked/kanso.nvim",
+    "zenbones-theme/zenbones.nvim",
+    dependencies = "rktjmp/lush.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme('kanso-zen')
+      vim.cmd.colorscheme('zenbones')
     end,
   },
 }
