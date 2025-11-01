aa7a72380926f89c40e5dbb953fd0bfc4a5a68ef
Daniel Hwang <dawh@ines.home>
2025-11-01 13:07:33 -0500

    Disable fish shell greeting

--- .config/fish/conf.d/prompt.fish
+++ .config/fish/conf.d/prompt.fish
@@ -1,3 +1,5 @@
+set -g fish_greeting
+
 function fish_prompt
     string join '' -- (set_color blue) $USER (set_color yellow) '@' (set_color magenta) $hostname ' ' (set_color green) (prompt_pwd --full-length-dirs 2) (set_color normal) ' '
 end
