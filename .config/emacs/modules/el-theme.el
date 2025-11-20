831beb07f92fbb5eb75100270117a5e0cfe04604
Daniel Hwang <dawh@ines.home>
2025-11-20 16:32:57 -0600

    Chore: reformat

--- .config/emacs/modules/el-theme.el
+++ .config/emacs/modules/el-theme.el
@@ -1,13 +1,13 @@
-;; -*- lexical-binding: t; -*-
-(use-package ef-themes
-  :ensure t
-  :init
-  (ef-themes-take-over-modus-themes-mode 1)
-  :config
-  (setq modus-themes-mixed-fonts t)
-  (setq modus-themes-italic-constructs t)
-  
-  (modus-themes-load-theme 'ef-dream)
-  )
+  ;; -*- lexical-binding: t; -*-
+  (use-package ef-themes
+    :ensure t
+    :init
+    (ef-themes-take-over-modus-themes-mode 1)
+    :config
+    (setq modus-themes-mixed-fonts t)
+    (setq modus-themes-italic-constructs t)
+    
+    (modus-themes-load-theme 'ef-dream)
+    )
 
-(provide 'el-theme)
+  (provide 'el-theme)
