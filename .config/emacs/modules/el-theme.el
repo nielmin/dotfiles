3e99929af5897b99b851d6ff11c306cdc2f3d59d
Daniel Hwang <dawh@ines.home>
2025-11-20 16:29:26 -0600

    Fix broken ef-themes

--- .config/emacs/modules/el-theme.el
+++ .config/emacs/modules/el-theme.el
@@ -2,8 +2,12 @@
 (use-package ef-themes
   :ensure t
   :init
+  (ef-themes-take-over-modus-themes-mode 1)
   :config
-  (ef-themes-select 'ef-dream)
+  (setq modus-themes-mixed-fonts t)
+  (setq modus-themes-italic-constructs t)
+  
+  (modus-themes-load-theme 'ef-dream)
   )
 
 (provide 'el-theme)
