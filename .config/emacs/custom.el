361a9d8d0326a9c91a5ffa8d3209e2573c2672ca
Daniel Hwang <dawh@ines.home>
2025-11-30 16:18:27 -0600

    Update custom.el

--- .config/emacs/custom.el
+++ .config/emacs/custom.el
@@ -4,10 +4,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("51fa6edfd6c8a4defc2681e4c438caf24908854c12ea12a1fbfd4d055a9647a3"
+     "8363207a952efb78e917230f5a4d3326b2916c63237c1f61d7e5fe07def8d378"
+     default))
  '(package-selected-packages
    '(consult corfu diminish direnv eat ef-themes exec-path-from-shell
-             flycheck go-mode magit marginalia orderless
-             rainbow-delimiters transient treesit-auto vertico)))
+             flycheck go-mode gruvbox-theme magit marginalia orderless
+             rainbow-delimiters treesit-auto vertico)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
