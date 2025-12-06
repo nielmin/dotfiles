99b1f049a5c93341b5709c3aeac31c9afcfeed87
Daniel Hwang <dawh@ines.home>
2025-12-06 15:49:30 -0600

    Fix doric-themes module

--- .config/emacs/modules/el-theme.el
+++ .config/emacs/modules/el-theme.el
@@ -1,15 +1,15 @@
   ;; -*- lexical-binding: t; -*-
   (use-package doric-themes
     :ensure t
+    :demand t
+    :config
+    (setq doric-themes-to-toggle '(doric-light doric-dark))
+    (setq doric-themes-to-rotate doric-themes-collection)
+    (doric-themes-select 'doric-obsidian)
     :bind
     (("<f5>" . doric-themes-rotate)
      ("C-<f5>" . doric-themes-select)
      ("M-<f5>" . doric-themes-load-random))
-    :config
-    (setq doric-themes-to-toggle '(doric-light doric-dark))
-    (setq doric-themes-to-rotate doric-themes-collection)
-
-    (doric-themes-select 'doric-obsidian)
     )
 
   (provide 'el-theme)
