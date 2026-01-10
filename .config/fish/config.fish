8f362475518fa9e69c0ca8686221db2774bc3191
Daniel Hwang <dawh@ines.home>
2026-01-10 16:16:10 -0600

    Add old emacs binds

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,9 @@ if status is-interactive
 end
 
 direnv hook fish | source
+
+bind alt-backspace backward-kill-word 
+bind ctrl-alt-h backward-kill-word 
+bind ctrl-backspace backward-kill-token 
+bind alt-delete kill-word 
+bind ctrl-delete kill-token
