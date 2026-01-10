adfbaaff9841ab5a0cdba689fdbbfd4eea8aadd1
Daniel Hwang <dawh@ines.home>
2026-01-10 17:53:21 -0600

    Update fish prompt

--- .config/fish/conf.d/prompt.fish
+++ .config/fish/conf.d/prompt.fish
@@ -1,5 +1,11 @@
 set -g fish_greeting
 
 function fish_prompt
-    string join '' -- (set_color blue) $USER (set_color yellow) '@' (set_color magenta) $hostname ' ' (set_color green) (prompt_pwd --full-length-dirs 2) (set_color normal) ' '
+    set -l last_status $status
+    # Prompt status only if it's not 0
+    set -l stat
+    if test $last_status -ne 0
+        set stat (set_color red)"[$last_status]"(set_color normal)
+    end
+    string join '' -- (set_color yellow) $hostname ' ' (set_color blue) (prompt_pwd --full-length-dirs 2) (set_color normal) ' ' $stat ' '
 end
