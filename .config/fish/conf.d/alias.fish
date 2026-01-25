90d3e81593a39c9f676dbfe3e26c627441951f4f
Daniel Hwang <dawh@ines.home>
2026-01-25 16:00:15 -0600

    Remove unused aliases

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -9,11 +9,3 @@ end
 function nv
   command nvim $argv
 end
-
-function butane
-  command podman run -i --rm -v "$PWD":/pwd quay.io/coreos/butane:release $argv
-end
-
-function podlet
-  command podman run -it --rm -v "$PWD":/pwd ghcr.io/containers/podlet $argv
-end
