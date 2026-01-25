21256d336c81da4a4818dd1a088b265a0113d4a8
Daniel Hwang <dawh@ines.home>
2026-01-25 16:56:47 -0600

    Fix abbreviations

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -1,2 +1,2 @@
-abbr ls "ls --color=auto --group-directories-first $argv"
-abbr ll "ls -ahl --color=auto --group-directories-first $argv"
+abbr ls "ls --color=auto --group-directories-first" $argv
+abbr ll "ls -ahl --color=auto --group-directories-first" $argv
