edff4e9f1db4e1b003c1f52c1856777ee76ddda8
Daniel Hwang <dawh@ines.home>
2026-01-28 11:01:27 -0600

    Update custom.el

--- .config/emacs/custom.el
+++ .config/emacs/custom.el
@@ -4,7 +4,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(consult corfu denote diminish direnv eat ef-themes
+             exec-path-from-shell flycheck go-mode jinx magit
+             marginalia orderless rainbow-delimiters treesit-auto
+             vertico)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
