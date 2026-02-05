07796c20aa0f23566fa69bf746b3d6244c575694
Daniel Hwang <dawh@ines.home>
2026-02-04 19:05:31 -0600

    Add back more emacs binds

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+bind ctrl-right forward-word
+bind ctrl-left backward-word
+bind alt-right nextd-or-forward-word
+bind alt-left prevd-or-backward-word
 bind alt-backspace backward-kill-word 
 bind ctrl-alt-h backward-kill-word 
 bind ctrl-backspace backward-kill-token 
