3a4cfeeda9afe72e51add5c6a8b64e16774c10cc
Daniel Hwang <dawh@ines.home>
2026-02-10 14:43:50 -0600

    Update theme to doric-themes

--- .config/emacs/modules/el-theme.el
+++ .config/emacs/modules/el-theme.el
@@ -1,15 +1,17 @@
-  ;; -*- lexical-binding: t; -*-
-  (use-package ef-themes
-    :init
-    (ef-themes-take-over-modus-themes-mode 1)
-    :config
-    (setq modus-themes-mixed-fonts t)
-    (setq modus-themes-italic-constructs t)
-    (modus-themes-load-theme 'ef-owl)
-    :bind
-    (("<f5>" . ef-themes-rotate)
-     ("C-<f5>" . ef-themes-select)
-     ("M-<f5>" . ef-themes-load-random))
-    )
+;; -*- lexical-binding: t; -*-
 
-  (provide 'el-theme)
+(use-package doric-themes
+  :demand t
+  :config
+  (setq doric-themes-to-toggle '(doric-marble doric-obsidian))
+  (setq doric-themes-to-rotate doric-themes-collection)
+  
+  (doric-themes-select 'doric-obsidian)
+  
+  :bind
+  (("<f5>" . doric-themes-toggle)
+   ("C-<f5>" . doric-themes-select)
+   ("M-<f5>" . doric-themes-rotate))
+  )
+
+(provide 'el-theme)
