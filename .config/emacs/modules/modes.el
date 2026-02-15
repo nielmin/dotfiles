9c604a616b3d7ba392d62958723974c239306930
Daniel Hwang <dawh@ines.home>
2026-02-14 22:13:34 -0600

    Add markdown-mode

--- .config/emacs/modules/modes.el
+++ .config/emacs/modules/modes.el
@@ -28,3 +28,9 @@
 )
 
 (provide 'modes)
+
+(use-package markdown-mode
+  :mode ("README\\.md\\'" . gfm-mode)
+  :init (setq markdown-command "multimarkdown")
+  :bind (:map markdown-mode-map
+              ("C-c C-e" . markdown-do)))
