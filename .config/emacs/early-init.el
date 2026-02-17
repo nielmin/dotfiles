280d7f60ae71384abe869846279841e5a3f0571e
Daniel Hwang <dawh@ines.home>
2026-02-16 18:10:04 -0600

    chore: clean up early-init.el

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,17 +1,16 @@
-  ;; early-init.el --- Emacs pre package.el & GUI configuration -*- lexical-binding: t; -*-
-  ;; Code:
-  (setopt backup-inhibited t
+;; early-init.el --- Emacs pre package.el & GUI configuration -*- lexical-binding: t; -*-
+;; Code:
+(setopt backup-inhibited t
   	  make-backup-files nil
-  	  auto-save-default nil)
+  	  auto-save-default nil
+        native-comp-async-report-warnings-errors nil
+        inhibit-splash-screen t
+        use-file-dialog nil
+        )
 
-  (setopt native-comp-async-report-warnings-errors nil)
-
-  ;; Unneeded UI elemenets
-  (menu-bar-mode -1)
-  (tool-bar-mode -1)      
-  (scroll-bar-mode -1)    
-  (tooltip-mode -1)       
-  (set-fringe-mode 10)
-
-  (setopt inhibit-splash-screen t)
-  (setopt use-file-dialog nil)
+;; Unneeded UI elemenets
+(menu-bar-mode -1)
+(tool-bar-mode -1)      
+(scroll-bar-mode -1)    
+(tooltip-mode -1)       
+(set-fringe-mode 10)
