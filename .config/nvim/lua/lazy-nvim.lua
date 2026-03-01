dba7fa4be298420348358982a8d308940f60b20f
Daniel Hwang <dawh@ines.home>
2026-02-28 21:00:46 -0600

    chore: flatten structure

--- .config/nvim/lua/lazy-nvim.lua
+++ .config/nvim/lua/lazy-nvim.lua
@@ -5,21 +5,16 @@ if not vim.loop.fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath,
   })
 end
+
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    { import = "plugins" },
-  },
-  {
-    checker = {
-      enabled = true,
-      notify = false,
+    spec = {
+        { import = "plugins" },
+
     },
-    change_detection = {
-      notify = false,
-    },
-  })
+})
