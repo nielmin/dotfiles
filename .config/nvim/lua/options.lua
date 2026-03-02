832c7fdeb9e2f38d57c01a87f28fc47e66786b08
Daniel Hwang <dawh@ines.home>
2026-03-01 20:42:09 -0600

    update: set tab width to 2 spaces

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -7,9 +7,9 @@ opt.ignorecase = true -- Ignore case in search
 opt.swapfile = false -- Disable swap files
 opt.autoindent = true -- Enable auto indentation
 opt.expandtab = true -- Use spaces instead of tabs
-opt.tabstop = 4 -- Number of spaces for a tab
-opt.softtabstop = 4 -- Number of spaces for a tab when editing
-opt.shiftwidth = 4 -- Number of spaces for autoindent
+opt.tabstop = 2 -- Number of spaces for a tab
+opt.softtabstop = 2 -- Number of spaces for a tab when editing
+opt.shiftwidth = 2 -- Number of spaces for autoindent
 opt.shiftround = true -- Round indent to multiple of shiftwidth
 opt.list = true -- Show whitespace characters
 opt.number = true -- Show line numbers
@@ -19,11 +19,10 @@ opt.wrap = false -- Disable line wrapping
 opt.cursorline = true -- Highlight the current line
 opt.scrolloff = 8 -- Keep 8 lines above and below the cursor
 opt.inccommand = "nosplit" -- Shows the effects of a command incrementally in the buffer
-opt.undodir = os.getenv('HOME') .. '/.vim/undodir' -- Directory for undo files
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir" -- Directory for undo files
 opt.undofile = true -- Enable persistent undo
 opt.completeopt = { "menuone", "popup", "noinsert" } -- Options for completion menu
 opt.winborder = "rounded" -- Use rounded borders for windows
 opt.hlsearch = false -- Disable highlighting of search results
 
 vim.cmd.filetype("plugin indent on") -- Enable filetype detection, plugins, and indentation
-
