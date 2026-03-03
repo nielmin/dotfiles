892abc22a64738829f4192347ccc490a4b54c5cc
Daniel Hwang <dawh@ines.home>
2026-03-02 20:56:32 -0600

    update: increas font size

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -13,7 +13,7 @@ config.initial_cols = 120
 config.initial_rows = 28
 
 -- or, changing the font size and color scheme.
-config.font_size = 10
+config.font_size = 18
 config.font = wezterm.font("Agave")
 config.color_scheme = "tokyonight"
 
