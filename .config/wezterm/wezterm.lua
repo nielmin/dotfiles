fdcf7f69da8d5e821199da61dbe38345960259ff
Daniel Hwang <dawh@ines.home>
2026-03-02 20:50:05 -0600

    initial: wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+config.enable_tab_bar = false
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
+config.font_size = 10
+config.font = wezterm.font("Agave")
+config.color_scheme = "tokyonight"
+
+-- Finally, return the configuration to wezterm:
+return config
